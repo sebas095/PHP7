524d12c2a2661944de8eae2dc3b9552794aa4b34
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-09-03 22:27:22 -0500

    if else

---
+++ temas/if.php
@@ -0,0 +1,20 @@
+<?php
+  $a = 15;
+  $b = 20;
+
+  // if
+  if ($a < $b):
+    echo "a es menor";
+  else:
+    echo "a es mayor";
+  endif;
+
+  // elseif
+  if ($a < $b):
+    echo "a es menor";
+  elseif ($a == b):
+    echo "a y b son iguales";
+  else:
+    echo "a es mayor";
+  endif;
+?>
