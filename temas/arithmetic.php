966c4d489de624ea8a1951d8107d28f9ec609c8f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-09-03 19:31:36 -0500

    arithmetic operators

---
+++ temas/arithmetic.php
@@ -0,0 +1,42 @@
+<?php
+  // Adición (Suma)
+  $a = 15;
+  $b = 5;
+  $suma = $a + $b;
+  echo "$suma \n";
+
+  // Sustracción (Resta)
+  $resta = $a - $b;
+  echo "$resta \n";
+
+  // Multiplicación
+  $multiplicacion = $a * $b;
+  echo "$multiplicacion \n";
+
+  // División
+  $division = $a / $b;
+  echo "$division \n";
+
+  // Modulo
+  $a = 47;
+  $b = 15;
+  $residuo = $a - ($b * 3);
+  $modulo = $a % $b;
+  echo "Residuo: $residuo \n";
+  echo "Modulo $modulo \n";
+
+  // Exponenciación
+  $a = 2;
+  $b = 3;
+  $exp = $a ** $b;
+  echo "$exp \n";
+
+  // Identidad y negacion
+  $identidad = +$resta;
+  echo "$resta \n";
+  echo "$identidad \n";
+
+  $negacion = -$resta;
+  echo "$resta \n";
+  echo "$identidad \n";
+?>
