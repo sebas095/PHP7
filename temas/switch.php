d0640bc2da6898f02baf10ccf6f00b389c5d54c7
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-10-22 16:58:34 -0500

    switch, while and do while

---
+++ temas/switch.php
@@ -0,0 +1,20 @@
+<?php
+  $i = 3;
+
+  switch ($i) {
+    case 1:
+      echo "Lunes";
+      break;
+
+    case 2:
+      echo "Martes";
+      break;
+
+    case 3:
+      echo "Miercoles";
+      break;
+
+    default:
+      echo "No existe";
+  }
+?>
